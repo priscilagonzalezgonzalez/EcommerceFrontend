7a7a48a618584265857b0415a8590391567f35db
priscilagonzalezgonzalez <psarahi.gonzalez@alumnos.udg.mx>
2025-11-25 14:00:36 -0600

    [ADD] to CartStore the state of total items in the cart is shown on top of the cart logo. Logic was also implemented.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,9 @@
 import { Link } from "@tanstack/react-router";
 import logo from "../assets/logo.png";
+import { useCartStore } from "../stores/useCartStore";
 
 const NavBar = () => {
-
+    const totalItems = useCartStore((state) => state.totalItems);
     return(
         <>
         <div className="flex justify-between px-10 items-center">
@@ -42,10 +43,14 @@ const NavBar = () => {
                 </svg>
                 </Link>
 
-                <Link to="/cart" className="[&.active]:font-bold">
+                <Link to="/cart" className="[&.active]:font-bold relative">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
+                    
+                    {totalItems > 0 && (
+                        <span className="poppins-medium text-base absolute -right-2 -top-4">{totalItems}</span>
+                    )}
                 </Link>
 
                 <Link to="/contact" className="[&.active]:font-bold">
