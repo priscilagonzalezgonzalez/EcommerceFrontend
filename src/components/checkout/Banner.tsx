7d27fee8d757aecb3e0385c57f2073094482f72e
priscilagonzalezgonzalez <psarahi.gonzalez@alumnos.udg.mx>
2025-11-25 17:56:29 -0600

    [FEAT] to Cart and Checkout banners. Refactor a banner component to be reusable through props

--- src/components/checkout/Banner.tsx
+++ src/components/checkout/Banner.tsx
@@ -1,32 +1,16 @@
-import { Link } from "@tanstack/react-router";
-import logo from "../../assets/logo.png";
-import background from "../../assets/banner.jpg"
+import Banner from "../../utils/components/Banner";
+import type { banner } from "../../schemas/banner.schema";
 
 const CheckoutBanner = () => {
+    const bannerProps : banner = {
+        mainTitle: 'Checkout',
+        subtitle: 'Checkout',
+        linkTo: '/checkout'
+    }
 
     return (
         <>
-        <div className="flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat h-[316px] opacity-50" style={{backgroundImage: `url(${background})`}}>
-            <img src={logo} alt="" className="h-[77px] block blur-none"/>
-            <h1 className="block poppins-medium text-5xl pb-5">Checkout</h1>
-            <div className="flex gap-2 items-center blur-none">
-                <Link to="/products" className="poppins-medium text-base">
-                    Home
-                </Link>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="size-4 ">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-
-                <div className="">
-                    <Link to="/checkout" className="poppins-medium text-base">
-                        Checkout
-                    </Link>
-                </div>
-
-            </div>
-            
-        </div>
+        <Banner {...bannerProps}/>
         </>
     )
 }
---
+++ src/utils/components/Banner.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@tanstack/react-router";
+import logo from "../../assets/logo.png";
+import background from "../../assets/banner.jpg"
+import { type banner } from "../../schemas/banner.schema";
+
+const Banner = (props: banner) => {
+
+    return (
+        <>
+        <div className="relative flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat h-[316px] before:absolute before:inset-0 before:bg-white before:opacity-50 before:content-['']" style={{
+            backgroundImage: `url(${background})`
+            }}>
+            
+            <img src={logo} alt="" className="h-[77px] block relative z-10" style={{}}/>
+            <strong className="block poppins-medium text-5xl pb-5 relative z-10">{props.mainTitle}</strong>
+            <div className="flex gap-2 items-center relative z-10">
+                <Link to="/products" className="poppins-medium text-base">
+                    Home
+                </Link>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="size-4 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+
+                <div className="">
+                    <Link to={props.linkTo} className="poppins-medium text-base">
+                        {props.subtitle}
+                    </Link>
+                </div>
+
+            </div>
+            
+        </div>
+        </>
+    )
+}
+
+export default Banner;
