df3562f947c17f2dd6c2c81808ab567bb8d34eac
priscilagonzalezgonzalez <psarahi.gonzalez@alumnos.udg.mx>
2025-12-02 17:38:14 -0600

    [ADD] to Checkout Button, disabled checkout when there're no items in the cart

--- src/components/cart/CheckoutButton.tsx
+++ src/components/cart/CheckoutButton.tsx
@@ -1,10 +1,16 @@
 import { Link } from "@tanstack/react-router";
+import { useCartStore } from "../../stores/useCartStore";
 
 const CheckoutButton = () => {
+    const totalItems = useCartStore((state) => state.totalItems);
+
     return (
         <>
         <Link to="/checkout">
-            <button className="border w-[222px] h-[56px] rounded-2xl poppins-regular text-xl cursor-pointer hover:bg-medium-gold hover:text-white">
+            <button 
+            disabled={totalItems === 0} 
+            className="border w-[222px] h-[56px] rounded-2xl poppins-regular text-xl cursor-pointer hover:bg-medium-gold hover:text-white
+            disabled:bg-gray-400 disabled:cursor-not-allowed">
                 Check Out
             </button>
         </Link>
